23ac9b22842ffd45ef8e3e66f3ee7874094474e5
Nathan R Smith <nrsmith012@gmail.com>
2018-01-06 14:04:07 -0800

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def new
+    u = User.create(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation] )
+    if u
+      session[:user_id] = u.id
+      redirect_to "/songs"
+    else
+      flash[:errors] = ["there was a problem with your username or password"]
+      session[:user_id] = nil
+      redirect_to '/main'
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  post 'users' => 'users#new'
 
   get 'users' => 'users#show'
 
   get 'users/edit'
 
-  get 'main' => 'sessions#new'
   get '' => 'sessions#new'
+  get 'main' => 'sessions#new'
   post 'sessions' => 'sessions#create'
+  post 'main/logout' => 'sessions#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
